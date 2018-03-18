793a7fb9fcb9d01d5dff3b44a57fd18ef2950923
Mike <Mdubbpro@gmail.com>
2018-03-18 14:59:18 -0700

    Multiple hotkeys working.

--- background.js
+++ background.js
@@ -1,6 +1,18 @@
-const store = {};
+chrome.commands.onCommand.addListener( command => {
+  let preset = command[6];
+  let store = {};
 
-chrome.storage.sync.get(null, result => {
-  console.log('My guess at the key is: ', result)
-  store = result.store
+  chrome.storage.sync.get(null, result => {
+    console.log('The current keys are: ', result)
+    store = result.store
+  });
+
+  chrome.tabs.getSelected(null, tab => {
+    // console.log('the current tab is: ', tab);
+    chrome.bookmarks.create({
+      parentId: store[preset].id,
+      title: tab.title,
+      url: tab.url
+    });
+  })
 });
