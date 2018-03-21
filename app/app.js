51fbd7533270258acd31e94852eeaf614fc6d346
Mike <Mdubbpro@gmail.com>
2018-03-20 17:52:17 -0700

    Refactored to work on mac (possibly).

--- app/app.js
+++ app/app.js
@@ -95,9 +95,15 @@ angular.module('hotmap', ['ui.bootstrap'])
         //important for lifehook cycle
         $timeout()
 
-        $(document).ready(function(e) {
-          $("#folders").focus();
+        $timeout(function() {
+          $(document).ready(function(e) {
+            $("#folders").focus();
+          });
         });
+
+        // $(document).ready(function(e) {
+        //   $("#folders").focus();
+        // });
 
       };
     },
@@ -124,7 +130,7 @@ angular.module('hotmap', ['ui.bootstrap'])
         <div class="container-fluid">
 
           <div class="input-group"  ng-if="!openpanel">
-              <input class="anysing.focus()" name="folders" id="folders" type="text" placeholder="enter a folder" ng-model="$parent.selected" uib-typeahead="bm as bm.title for bm in folders | filter:$viewValue | limitTo:8" class="form-control" typeahead-on-select="$ctrl.savebm($item)" ng-keypress="$ctrl.checkSubmit($event)">
+              <input name="folders" id="folders" type="text" placeholder="enter a folder" ng-model="$parent.selected" uib-typeahead="bm as bm.title for bm in folders | filter:$viewValue | limitTo:8" class="form-control" typeahead-on-select="$ctrl.savebm($item)" ng-keypress="$ctrl.checkSubmit($event)">
               <span class="input-group-addon"><span class="glyphicon glyphicon-saved" aria-hidden="true"></span></span>
           </div>
 
